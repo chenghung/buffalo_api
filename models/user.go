4242e2ce59a3425ced9fecab8a72ea8aee8cc27b
eddie.chen <chenghung0510@gmail.com>
2018-09-27 16:00:43 +0800

    fix(model): [User] name of db columns

--- models/user.go
+++ models/user.go
@@ -10,9 +10,9 @@ import (
 
 type User struct {
 	ID        int       `json:"id" db:"id"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
+	Email     string    `json:"email" db:"email"`
+	FirstName string    `json:"first_name" db:"first_name"`
+	LastName  string    `json:"last_name" db:"last_name"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
